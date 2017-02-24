457184c69e855e89d4fa34c46f54720e418ca9fb
Alex Bahler <alexbahler@gmail.com>
2017-02-24 03:24:46 +0000

    Enable root route; save port in variable

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+"use strict";
+
 const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
 
-// Main page
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// })
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+});
 
-app.listen(process.env.PORT || 8080);
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log('Server now running on port ', port);
+});
