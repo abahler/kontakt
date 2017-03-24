357732610230e7cbe469f4fd9c22cdb7d82124c8
Alex Bahler <alexbahler@gmail.com>
2017-03-24 01:17:05 +0000

    Change db (collection?) name in DATABASE_URL

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                            'mongodb://localhost/kontakt' :
+                            'mongodb://localhost/kontakt-dev');
                             
 exports.PORT = process.env.PORT || 8080;
