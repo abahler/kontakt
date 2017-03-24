f73a8f2407ce4ff0c5bb66e9639f27f8ed3e8b72
Alex Bahler <alexbahler@gmail.com>
2017-03-24 02:19:23 +0000

    Add function to connect to Mongo; include config and mongoose

--- server.js
+++ server.js
@@ -2,11 +2,38 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config');
 
-// this is a test comment
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
-app.use(express.static('public')); 
+// Set up MongoDB connection
+let runServer = (callback) => {
+    mongoose.connect(config.DATABASE_URL, (err) => {
+        if (err && callback) {
+            return callback(err);
+        }
 
+        app.listen(config.PORT, () => {
+            console.log('Listening on localhost:' + config.PORT);
+            if (callback) {
+                callback();
+            }
+        });
+    });
+};
+
+if (require.main === module) {
+    runServer(function(err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
+
+// Routes
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
 });
@@ -18,3 +45,4 @@ app.listen(port, () => {
 });
 
 exports.app = app;
+exports.runServer = runServer;
