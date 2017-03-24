38ca8f094a29f637d0b47891fee1d24ae930496b
Alex Bahler <alexbahler@gmail.com>
2017-03-24 02:24:10 +0000

    Remove redundant PORT and app.listen lines

--- server.js
+++ server.js
@@ -38,11 +38,5 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
 });
 
-let port = process.env.PORT || 8080;
-
-app.listen(port, () => {
-    console.log('Server now running on port ', port);
-});
-
 exports.app = app;
 exports.runServer = runServer;
