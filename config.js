c24a9e40624c313f0a58aa36fd05476270df7eb8
Alex Bahler <alexbahler@gmail.com>
2017-03-28 02:54:46 +0000

    Added production URL (MLab)

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/kontakt' :
+                            'mongodb://kontakt:replicant@ds143900.mlab.com:43900/kontakt' :
                             'mongodb://localhost/kontakt-dev');
                             
 exports.PORT = process.env.PORT || 8080;
