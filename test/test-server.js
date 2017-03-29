b88c55ce84fcd11b5ed10928abea14de8a0ad7fe
Alex Bahler <alexbahler@gmail.com>
2017-03-29 02:41:19 +0000

    Add (currently failing) test for GET request to /kontakts

--- test/test-server.js
+++ test/test-server.js
@@ -10,13 +10,23 @@ let app = server.app;
 chai.use(chaiHttp);
 
 describe('kontakt', () => {
-    
+
     it('should return a 200 on a GET for the root URL', (done) => {
         chai.request(app)
         .get('/')
         .end( (err, res) => {
             should.equal(err, null);
             res.should.have.status(200);
+            done();
+        });
+    });
+
+    it('should return a 201 on a GET for the kontakts listing', (done) => {
+        chai.request(app)
+        .get('/kontakts')
+        .end( (err, res) => {
+            should.equal(err, null);
+            res.should.have.status(201);
             done();
         });
     });
