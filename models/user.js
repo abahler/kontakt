6d3aca66f8cbd7613836c913a53b7b606d8e5a49
Alex Bahler <alexbahler@gmail.com>
2017-03-31 04:15:12 +0000

    Add 'kontakts' field that takes an array of Cards

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let mongoose = require('mongoose');
+let Card = require('./card');
 
 // Don't need to use an object if you're just declaring the data type, but do it for consistency
 // Sets a unique index, so you don't need the 'index' key
@@ -9,7 +10,8 @@ let UserSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     userName: { type: String, required: true },
     password: { type: String, required: true },
-    avatar: { type: String }
+    avatar: { type: String },
+    kontakts: { type: Array, "default": [] }
 });
 
 let User = mongoose.model('User', UserSchema);
